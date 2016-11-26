2a962918fcc6eff1438d492e2d1fb90cca760879
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2016-11-26 05:21:00 +0200

    Fix pip upgrade condition

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@ function before_install {
     export CXX=clang++
     get_macpython_environment $MB_PYTHON_VERSION venv
     source venv/bin/activate
-    if [ "$MB_PYTHON_VERSION" == "3.2" ]
+    if [ "$MB_PYTHON_VERSION" != "3.2" ]
     then
         pip install --upgrade pip
     fi
